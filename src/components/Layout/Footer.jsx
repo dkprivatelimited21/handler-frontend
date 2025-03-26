161a203392e910cd7c2521c8fe962634ed389812
unknown <everythingfree4200@gmail.com>
2025-03-26 12:31:45 +0530

    ansh

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -36,9 +36,7 @@ const Footer = () => {
       </div>
       <div className="grid grid-cols-1 sm:gird-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16 sm:text-center">
         <ul className="px-5 text-center sm:text-start flex sm:block flex-col items-center">
-          <img
-            src="https://shopo.quomodothemes.website/assets/images/logo.svg"
-            alt=""
+          <h1>Handler</h1>
             style={{ filter: "brightness(0) invert(1)" }}
           />
           <br />
