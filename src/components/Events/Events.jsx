b3367e1b3d381012d69a7b2c431f2f5408d4b423
unknown <everythingfree4200@gmail.com>
2025-03-27 18:09:40 +0530

    by updating

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -4,7 +4,8 @@ import styles from '../../styles/styles'
 import EventCard from "./EventCard";
 
 const Events = () => {
-  const allEvents,isLoading = useSelector((state) => state.events);  
+  const allEvents = useSelector((state) => state.events);
+  const isLoading = useSelector((state) => state.events);  
    
   return (
     <div>
