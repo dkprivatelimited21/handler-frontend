df01238d05fe9d63730228ffb714377249801d11
unknown <everythingfree4200@gmail.com>
2025-03-30 22:00:58 +0530

    14.0

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals.js";
 import { Provider } from "react-redux";
-import Store from "./redux/store";
+import Store from "./redux/store.js";
 
 ReactDOM.render(
   <Provider store={Store}>
