15fe40dc4d8c00778fc0939089c0da0b1e890bd1
unknown <everythingfree4200@gmail.com>
2025-03-31 22:43:26 +0530

    32.0

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -170,7 +170,23 @@ const Header = ({ activeHeading }) => {
               </div>
             </div>
 
-           
+            <div className={`${styles.noramlFlex}`}>
+              <div className="relative cursor-pointer mr-[15px]">
+                {isAuthenticated ? (
+                  <Link to="/profile">
+                    <img
+                      src={`${user?.avatar?.url}`}
+                      className="w-[35px] h-[35px] rounded-full"
+                      alt=""
+                    />
+                  </Link>
+                ) : (
+                  <Link to="/login">
+                    <CgProfile size={30} color="rgb(255 255 255 / 83%)" />
+                  </Link>
+                )}
+              </div>
+            </div>
 
             {/* cart popup */}
             {openCart ? <Cart setOpenCart={setOpenCart} /> : null}
