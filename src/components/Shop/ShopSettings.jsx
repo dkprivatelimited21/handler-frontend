897915faec67ee5f46f4dd1991f76e564f27d28a
unknown <everythingfree4200@gmail.com>
2025-04-04 22:55:26 +0530

    67.0

--- src/components/Shop/ShopSettings.jsx
+++ src/components/Shop/ShopSettings.jsx
@@ -107,7 +107,7 @@ const ShopSettings = () => {
             </div>
             <input
               type="name"
-              placeholder={`${seller.name}`}
+              placeholder={`${seller?.name}`}
               value={name}
               onChange={(e) => setName(e.target.value)}
               className={`${styles.input} !w-[95%] mb-4 800px:mb-0`}
