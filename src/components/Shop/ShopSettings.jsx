517ed6c9e032c6623d3e22b6f1d27d185d1a0061
unknown <everythingfree4200@gmail.com>
2025-04-04 22:50:01 +0530

    68.0

--- src/components/Shop/ShopSettings.jsx
+++ src/components/Shop/ShopSettings.jsx
@@ -77,7 +77,7 @@ const ShopSettings = () => {
         <div className="w-full flex items-center justify-center">
           <div className="relative">
             <img
-              src={avatar ? avatar : `${seller.avatar?.url}`}
+              src={avatar ? avatar : `${seller?.avatar?.url}`}
               alt=""
               className="w-[200px] h-[200px] rounded-full cursor-pointer"
             />
