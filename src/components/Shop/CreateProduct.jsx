4e8998c698158d97406a8e15177204b5be9e1f3e
unknown <everythingfree4200@gmail.com>
2025-04-05 00:35:12 +0530

    68.0

--- src/components/Shop/CreateProduct.jsx
+++ src/components/Shop/CreateProduct.jsx
@@ -74,7 +74,7 @@ const CreateProduct = () => {
         originalPrice,
         discountPrice,
         stock,
-        shopId: seller._id,
+        shopId: seller?._id,
         images,
       })
     );
