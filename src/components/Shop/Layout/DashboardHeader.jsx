85fb31e56ca870a6fb52b0fd79a86c4aece5db65
unknown <everythingfree4200@gmail.com>
2025-04-15 21:16:32 +0530

    7

--- src/components/Shop/Layout/DashboardHeader.jsx
+++ src/components/Shop/Layout/DashboardHeader.jsx
@@ -11,9 +11,11 @@ const DashboardHeader = () => {
   return (
     <div className="w-full h-[80px] bg-white shadow sticky top-0 left-0 z-30 flex items-center justify-between px-4">
       <div>
-        <Link to="/home">
-           <h1>local-handler</h1>
-        </Link>
+         <Link to="/">
+                      <h1 className={`text-[25px] leading-[1.2] 800px:text-[50px] text-[#3d3a3a] font-[600] capitalize` }>
+            local-handler
+            </h1>
+          </Link>
       </div>
       <div className="flex items-center">
         <div className="flex items-center mr-4">
