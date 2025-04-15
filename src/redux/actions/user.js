71ad998aea2c9fb9d7e4d43c0d24ca8cecb91d17
unknown <everythingfree4200@gmail.com>
2025-04-15 13:27:48 +0530

    92

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,47 +1,59 @@
+// user.js – cleaned and fixed version
 import axios from "axios";
 import { server } from "../../server";
 
-// load user
-export const loadUser = () => async (dispatch) => {
+// Register user
+export const registerUser = (user) => async (dispatch) => {
   try {
-    dispatch({
-      type: "LoadUserRequest",
-    });
-    const { data } = await axios.get(`${server}/user/getuser`, {
+    dispatch({ type: "RegisterUserRequest" });
+
+    const { data } = await axios.post(`${server}/user/create-user`, user, {
       withCredentials: true,
     });
-    dispatch({
-      type: "LoadUserSuccess",
-      payload: data.user,
-    });
+
+    dispatch({ type: "RegisterUserSuccess", payload: data.user });
   } catch (error) {
     dispatch({
-      type: "LoadUserFail",
-      payload: error.response.data.message,
+      type: "RegisterUserFail",
+      payload: error?.response?.data?.message || error.message,
     });
   }
 };
 
-// load seller
-export const loadSeller = () => async (dispatch) => {
+// Load user
+export const loadUser = () => async (dispatch) => {
   try {
-    dispatch({
-      type: "LoadSellerRequest",
-    });
-    const { data } = await axios.get(`${server}/shop/getSeller`, {
+    dispatch({ type: "LoadUserRequest" });
+
+    const { data } = await axios.get(`${server}/user/getuser`, {
       withCredentials: true,
     });
-    dispatch({
-      type: "LoadSellerSuccess",
-      payload: data.seller,
-    });
+
+    dispatch({ type: "LoadUserSuccess", payload: data.user });
   } catch (error) {
     dispatch({
-      type: "LoadSellerFail",
-      payload: error.response.data.message,
+      type: "LoadUserFail",
+      payload: error?.response?.data?.message || error.message,
     });
   }
 };
+
+// Logout
+export const logoutUser = () => async (dispatch) => {
+  try {
+    await axios.get(`${server}/user/logout`, {
+      withCredentials: true,
+    });
+
+    dispatch({ type: "LogoutSuccess" });
+  } catch (error) {
+    dispatch({
+      type: "LogoutFail",
+      payload: error?.response?.data?.message || error.message,
+    });
+  }
+};
+
 
 // user update information
 export const updateUserInformation =
@@ -115,29 +127,41 @@ export const updatUserAddress =
       });
     }
   };
-
-// delete user address
+// Delete user address
 export const deleteUserAddress = (id) => async (dispatch) => {
   try {
-    dispatch({
-      type: "deleteUserAddressRequest",
+    dispatch({ type: "DeleteUserAddressRequest" });
+
+    const { data } = await axios.delete(`${server}/user/delete-address/${id}`, {
+      withCredentials: true,
     });
 
-    const { data } = await axios.delete(
-      `${server}/user/delete-user-address/${id}`,
-      { withCredentials: true }
-    );
-
+    dispatch({ type: "DeleteUserAddressSuccess", payload: data.user });
+    dispatch({ type: "SuccessMessage", payload: "Address deleted successfully!" });
+  } catch (error) {
     dispatch({
-      type: "deleteUserAddressSuccess",
-      payload: {
-        successMessage: "User deleted successfully!",
-        user: data.user,
-      },
+      type: "DeleteUserAddressFail",
+      payload: error?.response?.data?.message || error.message,
+    });
+  }
+};
+
+// load seller
+export const loadSeller = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: "LoadSellerRequest",
+    });
+    const { data } = await axios.get(`${server}/shop/getSeller`, {
+      withCredentials: true,
+    });
+    dispatch({
+      type: "LoadSellerSuccess",
+      payload: data.seller,
     });
   } catch (error) {
     dispatch({
-      type: "deleteUserAddressFailed",
+      type: "LoadSellerFail",
       payload: error.response.data.message,
     });
   }
@@ -165,3 +189,4 @@ export const getAllUsers = () => async (dispatch) => {
     });
   }
 };
+
