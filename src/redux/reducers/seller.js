254f1ae360565cc8332338a5debda69525c9b2eb
unknown <everythingfree4200@gmail.com>
2025-04-17 13:08:17 +0530

    00

--- src/redux/reducers/seller.js
+++ src/redux/reducers/seller.js
@@ -3,28 +3,51 @@ import { createReducer } from "@reduxjs/toolkit";
 const initialState = {
   isLoading: true,
   seller: null,
+  sellers: [],
   error: null,
 };
 
 export const sellerReducer = (state = initialState, action) => {
   switch (action.type) {
+    // Load current logged-in seller
     case "LoadSellerRequest":
+    case "getAllSellersRequest":
       return {
         ...state,
         isLoading: true,
       };
+
     case "LoadSellerSuccess":
       return {
+        ...state,
         isLoading: false,
         seller: action.payload,
         error: null,
       };
-    case "LoadSellerFail":
+
+    case "getAllSellersSuccess":
       return {
+        ...state,
+        isLoading: false,
+        sellers: action.payload,
+        error: null,
+      };
+
+    case "LoadSellerFail":
+    case "getAllSellerFailed":
+      return {
+        ...state,
         isLoading: false,
         seller: null,
         error: action.payload,
       };
+
+    case "clearErrors":
+      return {
+        ...state,
+        error: null,
+      };
+
     default:
       return state;
   },
