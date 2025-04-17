3d27303778bb78b905b474c99570c87032487456
unknown <everythingfree4200@gmail.com>
2025-04-17 21:25:30 +0530

    000

--- src/static/data.js
+++ src/static/data.js
@@ -105,7 +105,7 @@ export const footerProductLinks = [
   { name: "Reviews", link: "/reviews" },
 ];
 
-export const footerCompanyLinks = [
+export const footercompanyLinks = [
   { name: "Shirts" },
   { name: "Shoes" },
   { name: "Accessories" },
