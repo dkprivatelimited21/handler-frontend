bba3bfec090c977f23e8bbca031b5181fd56b720
unknown <everythingfree4200@gmail.com>
2025-04-20 18:24:13 +0530

    000

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -63,9 +63,11 @@ const Payment = () => {
     }
 
     try {
-      const { data: keyData } = await axios.get("${server}/api/v2/payment/razorpay-key");
+      const { data: keyData } = await axios.get(`${server}/payment/razorpay-key`);
+
       const orderPayload = { amount: totalPrice * 100 };
-      const { data } = await axios.post("${server}/api/v2/payment/razorpay-checkout", orderPayload);
+      const { data } = await axios.post(`${server}/payment/razorpay-checkout`, orderPayload)
+;
 
       const options = {
         key: keyData.key,
@@ -90,7 +92,7 @@ const Payment = () => {
             razorpaySignature: response.razorpay_signature,
           };
 
-          const confirm = await axios.post('${server}/api/order/create-order', payload);
+          const confirm = await axios.post(`${server}/order/create-order`, payload);
 
           if (confirm.data.success) {
             toast.success("Payment successful and order placed!");
