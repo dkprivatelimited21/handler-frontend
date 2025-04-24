92768778b00f03b5f7281fde23d0b79f70d0d49f
unknown <everythingfree4200@gmail.com>
2025-04-24 08:19:41 +0530

    90

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -102,12 +102,11 @@ const Payment = () => {
             >
               {loading ? "Processing..." : "Pay Now"}
             </button>
-            {loading && (
               {loading && (
   <div className="flex justify-center mt-4">
     <div className="w-12 h-12 border-4 border-t-4 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
   </div>
-)}
+          )}
 
           </div>
         </div>
